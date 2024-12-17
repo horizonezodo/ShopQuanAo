e93b0dc14f7b466404f6273b66fda3ad7f4008ad
horizonezodo <thiensutoloi3@gmail.com>
2024-12-17 14:02:21 +0700

    u

--- src/main/java/vn/horizonezodo/core/Controller/HomeController.java
+++ src/main/java/vn/horizonezodo/core/Controller/HomeController.java
@@ -1,13 +1,14 @@
 package vn.horizonezodo.core.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(Model model){
-        return "index";
+    public ModelAndView index(){
+        ModelAndView model = new ModelAndView("index");
+        return model;
     }
 }
